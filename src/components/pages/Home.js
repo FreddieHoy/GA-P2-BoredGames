18e1c56a476bba0510960da3daafa6a5b001db88
Freddie Hoy <freddiehoy@hotmail.com>
2019-09-24 14:57:23 +0100

    deploy

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <h1 className="title has-text-black is-1 level-item">Bored Games </h1>
         <p className="subtitle has-text-black is-3 level-item">All board games 🎲</p>
         <hr />
-        <br /> HELLOOOOOOOO
+        <br />
         <p className="subtitle has-text-black is-4 level-item"> A useful bored game API for when you want to find a fun board game!</p>
         <p className="subtitle has-text-black is-4 level-item"> Have Fun! 😁 </p>
 
